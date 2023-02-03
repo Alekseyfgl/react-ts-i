8ea8cf06237ac5954065433d9369ab81771d811d
aleksey <Aleksey.fgl@gmail.com>
2023-02-03 19:38:40 +0300

    query

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import React, {useState} from 'react';
-import './App.css';
-import {NavLink, Outlet, Route, Routes} from "react-router-dom";
+import React, {useEffect} from 'react';
+import s from './App.module.css'
+import {Navigate, NavLink, Route, Routes, useNavigate, useSearchParams,} from 'react-router-dom';
 
 
-function App() {
+export function Profile() {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    console.log(searchParams.get('name'))
+    console.log(Object.fromEntries(searchParams))
 
     return (
         <div>
+            <button onClick={() => {
+                setSearchParams({...Object.fromEntries(searchParams), age: '34'})
+            }}>
+                add age
+            </button>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className={s.li}>
             <NavLink to={'/'}>main</NavLink>
             <NavLink to={'/login'}>login</NavLink>
             <NavLink to={'/profile'}>profile</NavLink>
             <NavLink to={'/profile/settings'}>settings</NavLink>
 
+
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>main</div>}/>
                 <Route path={'/login'} element={<div>login</div>}/>
-                <Route path={'/profile'} element={(
-                    <div>
-                        profile
-                        <Outlet/>
-                    </div>
-                )}>
-
-                    <Route path={'*'} element={<div>profile page not found</div>}/>
-                    <Route path={':id'} element={<div>id</div>}/>
-                    <Route path={'settings'} element={<div>settings</div>}/>
-
-                </Route>
-
+                <Route path={'/profile'} element={<div><Profile/></div>}/>
+                <Route path={'/profile/settings'} element={<div>settings</div>}/>
             </Routes>
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import App from './App';
+import {BrowserRouter} from 'react-router-dom';
+import {DevSupport} from '@react-buddy/ide-toolbox';
+import {ComponentPreviews, useInitial} from './dev';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <BrowserRouter>
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
             <App/>
+        </DevSupport>
     </BrowserRouter>
 );
 
